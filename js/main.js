232409874a9d7597b7a181ad15033d717b5a87be
katherine montecinos <katherine.montecinos.k@gmail.com>
2016-11-05 00:06:39 -0300

    modifique un poco el html y intente hacer las funciones

--- js/main.js
+++ js/main.js
@@ -3,27 +3,41 @@ function agregaTarea() {
   var contenedorLista = document.getElementById('contlista');
   var lista = document.createElement('div');
 
-  
   var elementoCheck = document.createElement('input');
   elementoCheck.type ='checkbox';
-
-  var elementoLista = document.createElement('p');
+  
 
   var elementoBorrar = document.createElement('span');
   elementoBorrar.setAttribute("class", "glyphicon glyphicon-trash pull-right");
 
+  var elementoLista = document.createElement('p');
+
   var nodoTarea = document.createTextNode(tarea);
 
   elementoLista.appendChild(nodoTarea);
-
   
   lista.appendChild(elementoCheck);
   lista.appendChild(elementoLista);
   lista.appendChild(elementoBorrar);
 
-  lista.setAttribute("class", "elementoCheck", "lista", "elementoBorrar");
+  lista.setAttribute("class", "lista","elementoCheck", "elementoBorrar");
 
   contenedorLista.appendChild(lista);
 
+}
+
+  elementoCheck.onclick = tacharTarea;
+
+function tacharTarea() {
+  
+  checkbox.classList.toggle('tachado');
+}
+
+
+function borrar(){
+  var borrar = document.querySelector('span');
+  borrar.addEventListener('click', function(){
+    div.removeChild(lista);
+  })
 }
 
